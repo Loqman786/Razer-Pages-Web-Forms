eb6d7b19660fa7f1444fa1be37fc02670e7ad951
Loqman786 <91545135+Loqman786@users.noreply.github.com>
2022-11-17 14:20:39 +0000

    upload web forms code

---
+++ razer pages/Pages/Index.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace razer_pages.Pages
+{
+    public class IndexModel : PageModel
+    {
+        [BindProperty]
+        public string FullName { get; set; }
+        
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            FullName = "Mohammed";
+        }
+
+        public void Onpost()
+        {
+            if(String.IsNullOrWhiteSpace(FullName))
+            {
+                ViewData["NoName"] = "You have not entered a name!";
+                FullName = "Anonymous";
+            }
+            else
+            {
+                ViewData["Messege"] = "Name is Registered!";
+                // Register the user
+            }
+        }
+    }
+}
